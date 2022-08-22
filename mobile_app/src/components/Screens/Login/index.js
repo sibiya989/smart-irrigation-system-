806e3eaca14a75923e97cdf0adff40e5cd6ac750
Mxolisi <ngwenyaharris34@gmail.com>
2022-08-22 08:37:03 +0200

    Added screen styles

---
+++ mobile_app/src/components/Screens/Login/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Styles from '../../Strings/globalStyles'
+
+export default function Login() {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  )
+}
---
+++ mobile_app/src/components/Strings/globalStyles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native'
+
+const Styles = StyleSheet.create({
+    LoginStyles:{
+        
+    }
+})
+
+export default Styles;
