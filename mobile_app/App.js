a6c6e92cc6db4a0c5f0494f99482dc0c7b9a92ca
HarrisMx <ngwenyaharris34@gmail.com>
2022-08-25 09:25:54 +0200

    Added firebase configurations

--- mobile_app/App.js
+++ mobile_app/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 const App = () => {
   return (
     <View>
-     
+      <Text>Hello App</Text>
     </View>
   );
 };
